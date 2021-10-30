d1faeb622cc2c36e54e7b32a4235fb5f965e9b36
Kristin <Kristineliseboyd13@gmail.com>
2021-10-29 19:51:36 -0700

    completes up to comparision operators

---
+++ starter/assignments.js
@@ -0,0 +1,13 @@
+const country = 'United States of America';
+const continent = 'North America';
+let population = '333,558,957';
+
+const isIsland = false 
+let language = 'English'
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+let description = ''
---
+++ starter/script.js
@@ -0,0 +1,54 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Kristin:');
+// console.log(29);
+
+// let firstName = 'Molly';
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+// console.log(myFirstJob);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof 23);
+// console.log(typeof 'kristin');
+
+
+// // Math Operators 
+const now = 2021
+const ageKristin = now - 1992;
+const ageMolly = now - 2014;
+console.log(ageKristin, ageMolly);
+
+console.log(ageKristin * 2, ageMolly / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Kristin";
+const lastName = "Boyd";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators 
+let x = 10 + 5; // 15
+x += 10; // x = x = 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 
+x--; // x - 1
+console.log(x); 
+
+// Comparison Operators <, >, >=, <= 
+console.log(ageKristin > ageMolly);
+console.log(ageKristin >= 18);
+
