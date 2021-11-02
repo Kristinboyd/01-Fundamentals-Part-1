f46874b0b676111400f744f695bcf32ac7ddd684
Kristin <Kristineliseboyd13@gmail.com>
2021-11-02 14:03:41 -0700

    completes Coding Challenge #4

--- starter/script.js
+++ starter/script.js
@@ -297,3 +297,9 @@
 
 // Coding Challenge #4
 // ****************************************************************
+// const bill = 275;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip}, and the total value was ${bill +
+//     tip}`
+// );
